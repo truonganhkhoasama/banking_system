4d68c09f17739366ee0df94a6d67b64710ad832d
21120272 <truonganhkhoacand@gmail.com>
2025-07-03 21:44:14 +0700

    bank

--- routes/banks/bankIntegration.routes.js
+++ routes/banks/bankIntegration.routes.js
@@ -2,12 +2,16 @@ import express from 'express';
 const router = express.Router();
 
 // import { verifySignature } from '../middlewares/verifySignature.js';
-import { queryAccountInfo, queryExternalAccountInfo } from '../../controllers/bankController.js';
+import { depositToAccount, externalDepositToLinkedBank, initiateExternalTransfer, queryAccountInfo, queryExternalAccountInfo } from '../../controllers/bankController.js';
+import authenticateToken from '../../middleware/authMiddleware.js';
+import authorizeRoles from '../../middleware/authorizeRoles.js';
 
 router.post('/account-info', queryAccountInfo);
 router.get('/query/:bank_code/:account_number', queryExternalAccountInfo);
+router.post('/deposit', depositToAccount);
+router.post('/transfer/initiate', authenticateToken, authorizeRoles('customer'), initiateExternalTransfer);
+router.post('/transfer/confirm', authenticateToken, authorizeRoles('customer'), externalDepositToLinkedBank);
 
-// router.post('/deposit', verifySignature, depositFromExternalBank);
 
 export default router;
 
